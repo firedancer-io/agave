54e422fae1a5834d1b283cc10c94acd183e76190
Michael McGee <mmcgee@jumptrading.com>
2023-10-06 21:02:13 +0000

    firedancer: build validator binaries as libraries
    We want to link the validator and genesis binaries into our Firedancer
    binary for zero configuration and easy deployment. We also don't want
    to rename the main.rs files, since that would cause repeated merge
    conflicts and make it hard to tell what Firedancer has changed. To do
    this, we point lib.rs at main.rs (main is a module), and then don't try
    build the binary.

--- genesis/src/main.rs
+++ genesis/src/main.rs
@@ -24,7 +24,9 @@ use {
     solana_epoch_schedule::EpochSchedule,
     solana_feature_gate_interface as feature,
     solana_fee_calculator::FeeRateGovernor,
-    solana_genesis::{
+    // FIREDANCER: We have inverted the dependency, so the library depends on main.rs so that this
+    // import now just needs to refer to the crate.
+    crate::{
         genesis_accounts::add_genesis_accounts, Base64Account, StakedValidatorAccountInfo,
         ValidatorAccountsFile,
     },
@@ -284,8 +286,15 @@ fn rent_exempt_check(stake_lamports: u64, exempt: u64) -> io::Result<()> {
     }
 }
 
+// FIREDANCER: Switch main to be a function that takes arguments, rather than
+// an actual entrypoint for the binary.
 #[allow(clippy::cognitive_complexity)]
-fn main() -> Result<(), Box<dyn error::Error>> {
+pub fn main<I, T>(itr: I) -> Result<(), Box<dyn error::Error>>
+where
+        I: IntoIterator<Item = T>,
+        T: Into<std::ffi::OsString> + Clone {
+    let args: Vec<std::ffi::OsString> = itr.into_iter().map(|x| x.into()).collect();
+
     let default_faucet_pubkey = solana_cli_config::Config::default().keypair_path;
     let fee_rate_governor = FeeRateGovernor::default();
     let (
@@ -607,7 +616,8 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                     feature sets",
                 ),
         )
-        .get_matches();
+        // FIREDANCER: Parse matches from the provided arguments rather than env_os()
+        .get_matches_from(args);
 
     let ledger_path = PathBuf::from(matches.value_of("ledger_path").unwrap());
 
