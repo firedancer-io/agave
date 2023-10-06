5a331c935ec55fec38f52b64ddf33f8e15fd8aed
Michael McGee <mmcgee@jumptrading.com>
2023-10-06 21:02:13 +0000

    firedancer: build validator binaries as libraries
    We want to link the validator and genesis binaries into our Firedancer
    binary for zero configuration and easy deployment. We also don't want
    to rename the main.rs files, since that would cause repeated merge
    conflicts and make it hard to tell what Firedancer has changed. To do
    this, we point lib.rs at main.rs (main is a module), and then don't try
    build the binary.

--- genesis/src/main.rs
+++ genesis/src/main.rs
@@ -16,7 +16,9 @@ use {
         },
     },
     solana_entry::poh::compute_hashes_per_tick,
-    solana_genesis::{genesis_accounts::add_genesis_accounts, Base64Account},
+    // FIREDANCER: We have inverted the dependency, so the library depends on main.rs so that this
+    // import now just needs to refer to the crate.
+    crate::{genesis_accounts::add_genesis_accounts, Base64Account},
     solana_ledger::{blockstore::create_new_ledger, blockstore_options::LedgerColumnOptions},
     solana_rpc_client::rpc_client::RpcClient,
     solana_rpc_client_api::request::MAX_MULTIPLE_ACCOUNTS,
@@ -176,8 +178,15 @@ fn features_to_deactivate_for_cluster(
     Ok(features_to_deactivate)
 }
 
+// FIREDANCER: Switch main to be a function that takes arguments, rather than
+// an actual entrypoint for the binary.
 #[allow(clippy::cognitive_complexity)]
-fn main() -> Result<(), Box<dyn error::Error>> {
+pub fn main<I, T>(itr: I) -> Result<(), Box<dyn error::Error>>
+where
+        I: IntoIterator<Item = T>,
+        T: Into<std::ffi::OsString> + Clone {
+    let args: Vec<std::ffi::OsString> = itr.into_iter().map(|x| x.into()).collect();
+
     let default_faucet_pubkey = solana_cli_config::Config::default().keypair_path;
     let fee_rate_governor = FeeRateGovernor::default();
     let (
@@ -491,7 +500,8 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                     feature sets",
                 ),
         )
-        .get_matches();
+        // FIREDANCER: Parse matches from the provided arguments rather than env_os()
+        .get_matches_from(args);
 
     let ledger_path = PathBuf::from(matches.value_of("ledger_path").unwrap());
 
