70742998c326b2cd0ddf06c1744a2d26478ec544
Michael McGee <mmcgee@jumptrading.com>
2024-01-12 22:38:51 +0000

    firedancer: use firedancer poh recorder

--- poh/src/lib.rs
+++ poh/src/lib.rs
@@ -1,5 +1,17 @@
 #![allow(clippy::arithmetic_side_effects)]
-pub mod poh_recorder;
+// FIREDANCER: Replace PohRecorder completely with one that goes out to
+// our implementation.
+// pub mod poh_recorder;
+mod firedancer_poh_recorder;
+pub mod old_poh_recorder;
+
+pub mod poh_recorder {
+  pub use crate::firedancer_poh_recorder::*;
+  // use everything except PohRecorder from old_poh_recorder
+  pub use crate::old_poh_recorder::{WorkingBankEntry, PohRecorderError, GRACE_TICKS_FACTOR, MAX_GRACE_SLOTS, PohLeaderStatus, Record, SharedWorkingBank, SharedTickHeight, SharedLeaderFirstTickHeight };
+  pub use crate::transaction_recorder::{RecordTransactionsSummary, RecordTransactionsTimings, TransactionRecorder};
+}
+
 pub mod poh_service;
 pub mod transaction_recorder;
 
