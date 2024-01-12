18efe41bfbcbb2496f2bb7e05dd0b8aa8aa89d91
Michael McGee <mmcgee@jumptrading.com>
2024-01-12 22:40:25 +0000

    firedancer: provide ffi for txn execute and commit

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -511,6 +511,13 @@ impl BankingStage {
     ) -> Result<Vec<JoinHandle<()>>, ()> {
         info!("Spawning internal central scheduler");
 
+        // FIREDANCER: The Firedancer PoH tile needs access to a Committer object
+        // to reuse the code in there for committing transactions. We just store
+        // one in a global here on boot.
+        committer::FIREDANCER_COMMITTER.store(
+          Box::into_raw(Box::new(self.committer.clone())) as *const Committer as u64,
+          Ordering::Release,
+        );
         assert!(num_workers <= BankingStage::max_num_workers());
         let num_workers = num_workers.get();
         const NUM_CHECK_WORKERS: NonZeroUsize = NonZeroUsize::new(8).unwrap();
