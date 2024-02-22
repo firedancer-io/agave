7f69a808b954de3f42044adad437e1c54be26c78
Michael McGee <mmcgee@jumptrading.com>
2024-02-22 22:13:12 +0000

    firedancer: send stake weights to firedancer
    The leader schedule cache is updated so that it has an optional path to
    send stake weights to firedancer.  This is done on a mcache because we
    do not want to drop or miss any recomputations, otherwise Firedancer
    will not recover.  The pack tile uses stake weights to compute the
    leader schedule, and the shred tile uses stake weights to compute shred
    destinations.

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -2237,6 +2237,8 @@ fn load_blockstore(
                 .map(|service| service.sender()),
             accounts_update_notifier,
             exit,
+            // FIREDANCER: We want to get leader scheduel updates from bank forks.
+            true,
         )
         .map_err(|err| err.to_string())?;
 
