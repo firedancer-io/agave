19f2c22111757623fe6fd5d462fdde6c3bb8aa22
Michael McGee <mmcgee@jumptrading.com>
2024-02-22 21:59:36 +0000

    firedancer: communicate cluster contact info
    The Firedancer shredder needs to know which nodes in the cluster
    to forward shreds to, which information is currently maintained
    by the Solana Labs cluster info. For now, we communicate this
    across the IPC boundary.

--- gossip/src/gossip_service.rs
+++ gossip/src/gossip_service.rs
@@ -50,6 +50,8 @@ impl GossipService {
         should_check_duplicate_instance: bool,
         stats_reporter_sender: Option<Sender<Box<dyn FnOnce() + Send>>>,
         exit: Arc<AtomicBool>,
+        // FIREDANCER: If this gossip service should send updates to Firedancer
+        send_firedancer: bool,
     ) -> Self {
         let (request_sender, request_receiver) =
             EvictingSender::new_bounded(GOSSIP_CHANNEL_CAPACITY);
@@ -90,6 +92,8 @@ impl GossipService {
             response_sender.clone(),
             should_check_duplicate_instance,
             exit.clone(),
+            // FIREDANCER: If this gossip service should send updates to Firedancer
+            send_firedancer,
         );
         let t_gossip = cluster_info.clone().gossip(
             bank_forks.clone(),
@@ -383,6 +387,8 @@ pub fn make_gossip_node(
         should_check_duplicate_instance,
         None,
         exit,
+        // FIREDANCER: No need to send to Firedancer from gossip spy
+        false,
     );
     (gossip_service, ip_echo, cluster_info)
 }
