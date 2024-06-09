94d7ac68a69ff7e908c8ba994e84cdc953380b1f
Max Kaplan <max@maxkaplan.me>
2024-06-09 10:05:06 -0400

    TECH-339 (solana): updating mods to handle 1.18.23

--- core/src/next_leader.rs
+++ core/src/next_leader.rs
@@ -10,8 +10,21 @@ use {
 pub(crate) fn next_leader_tpu_vote(
     cluster_info: &ClusterInfo,
     poh_recorder: &RwLock<PohRecorder>,
-) -> Option<(Pubkey, SocketAddr)> {
-    next_leader(cluster_info, poh_recorder, ContactInfo::tpu_vote)
+) -> Option<(Pubkey, std::net::SocketAddr)> {
+    next_leader_n(
+        cluster_info,
+        poh_recorder,
+        FORWARD_TRANSACTIONS_TO_LEADER_AT_SLOT_OFFSET,
+        ContactInfo::tpu_vote,
+    )
+}
+
+pub(crate) fn next_leader_tpu_vote_maybe_vote_n(
+    cluster_info: &ClusterInfo,
+    poh_recorder: &RwLock<PohRecorder>,
+    n: u64,
+) -> Option<(Pubkey, std::net::SocketAddr)> {
+    next_leader_n(cluster_info, poh_recorder, n, ContactInfo::tpu_vote)
 }
 
 pub(crate) fn next_leader<F, E>(
@@ -22,10 +35,24 @@ pub(crate) fn next_leader<F, E>(
 where
     F: FnOnce(&ContactInfo) -> Result<SocketAddr, E>,
 {
-    let leader_pubkey = poh_recorder
-        .read()
-        .unwrap()
-        .leader_after_n_slots(FORWARD_TRANSACTIONS_TO_LEADER_AT_SLOT_OFFSET)?;
+    next_leader_n(
+        cluster_info,
+        poh_recorder,
+        FORWARD_TRANSACTIONS_TO_LEADER_AT_SLOT_OFFSET,
+        port_selector,
+    )
+}
+
+fn next_leader_n<F, E>(
+    cluster_info: &ClusterInfo,
+    poh_recorder: &RwLock<PohRecorder>,
+    n: u64,
+    port_selector: F,
+) -> Option<(Pubkey, SocketAddr)>
+where
+    F: FnOnce(&ContactInfo) -> Result<SocketAddr, E>,
+{
+    let leader_pubkey = poh_recorder.read().unwrap().leader_after_n_slots(n)?;
     cluster_info
         .lookup_contact_info(&leader_pubkey, port_selector)?
         .map(|addr| (leader_pubkey, addr))
