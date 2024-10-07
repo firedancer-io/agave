aaf15ddf0b89fc101c69f168f13e3da9b9573a34
Michael McGee <mmcgee@jumptrading.com>
2024-10-07 20:51:31 +0000

    firedancer: gui, add error count for non-vote transactions
    Co-authored-by: Yufeng Zhou <yufzhou@jumptrading.com>

--- core/src/banking_stage/consumer.rs
+++ core/src/banking_stage/consumer.rs
@@ -622,6 +622,7 @@ impl Consumer {
             executed_transactions_count,
             executed_non_vote_transactions_count,
             executed_with_successful_result_count,
+            executed_non_vote_with_successful_result_count,
             signature_count,
             error_counters,
             ..
@@ -703,6 +704,7 @@ impl Consumer {
                 executed_transactions_count,
                 executed_non_vote_transactions_count,
                 executed_with_successful_result_count,
+                executed_non_vote_with_successful_result_count,
             )
         } else {
             (
