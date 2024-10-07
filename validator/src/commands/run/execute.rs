6fca02cff41aa925920788145a9989ea3e0ff359
Michael McGee <mmcgee@jumptrading.com>
2024-10-07 20:59:16 +0000

    firedancer: gui, send startup progress messages

--- validator/src/commands/run/execute.rs
+++ validator/src/commands/run/execute.rs
@@ -1032,7 +1032,7 @@ pub fn execute(
     let mut ledger_lock = ledger_lockfile(&ledger_path);
     let _ledger_write_guard = lock_ledger(&ledger_path, &mut ledger_lock);
 
-    let start_progress = Arc::new(RwLock::new(ValidatorStartProgress::default()));
+    let start_progress = Arc::new(solana_core::validator::VSPRwLock::new());
     let admin_service_post_init = Arc::new(RwLock::new(None));
     let (rpc_to_plugin_manager_sender, rpc_to_plugin_manager_receiver) =
         if starting_with_geyser_plugins {
@@ -1138,6 +1138,7 @@ pub fn execute(
     );
 
     if !cluster_entrypoints.is_empty() {
+        *start_progress.write().unwrap() = ValidatorStartProgress::Initializing;
         bootstrap::rpc_bootstrap(
             &node,
             &identity_keypair,
@@ -1155,7 +1156,6 @@ pub fn execute(
             maximum_snapshot_download_abort,
             run_args.socket_addr_space,
         );
-        *start_progress.write().unwrap() = ValidatorStartProgress::Initializing;
     }
 
     if operation == Operation::Initialize {
