ff3334e47aef9a2ee9ba7929beec3770c6312222
Ravyu Sivakumaran <rsivakumaran@jumptrading.com>
2025-05-07 15:12:57 +0000

    expose TransactionAccounts::get

--- transaction-context/src/lib.rs
+++ transaction-context/src/lib.rs
@@ -97,7 +97,8 @@ impl TransactionAccounts {
         self.accounts.len()
     }
 
-    fn get(&self, index: IndexOfAccount) -> Option<&RefCell<AccountSharedData>> {
+    /* For use in vm syscall/interpreter fuzzer */
+    pub fn get(&self, index: IndexOfAccount) -> Option<&RefCell<AccountSharedData>> {
         self.accounts.get(index as usize)
     }
 
