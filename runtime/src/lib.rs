25934b0827cf4636ee60432ee389f54182fdcc6a
Manik Jain <mjain@jumptrading.com>
2025-06-05 15:48:04 +0000

    Make stake_account module public

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -29,7 +29,7 @@ pub mod snapshot_hash;
 pub mod snapshot_minimizer;
 pub mod snapshot_package;
 pub mod snapshot_utils;
-mod stake_account;
+pub mod stake_account;
 pub mod stake_history;
 pub mod stake_weighted_timestamp;
 pub mod stakes;
