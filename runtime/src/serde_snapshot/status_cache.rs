dcb36114c4d19f60c6407ec298945fa217572e51
Tom Pointon <topointon@jumptrading.com>
2025-06-24 22:44:54 +0000

    FD-Agave patches for fuzzing

--- runtime/src/serde_snapshot/status_cache.rs
+++ runtime/src/serde_snapshot/status_cache.rs
@@ -430,6 +430,7 @@ impl From<SerdeInstructionError> for InstructionError {
 }
 
 impl From<InstructionError> for SerdeInstructionError {
+    #[allow(deprecated)]
     fn from(err: InstructionError) -> Self {
         match err {
             InstructionError::GenericError => Self::GenericError,
