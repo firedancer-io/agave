4f375318b2d27348608dd42b061ce12690bab05a
Alex Peng <alpeng@jumptrading.com>
2026-03-06 05:49:19 +0000

    firedancer: send executed transaction hashes

--- runtime/src/transaction_execution.rs
+++ runtime/src/transaction_execution.rs
@@ -105,6 +105,21 @@ pub fn execute_batch<'a>(
         replay_vote_send_type,
     );
 
+    // FIREDANCER: Publish executed transaction hashes
+    for txn in commit_results
+        .iter()
+        .zip(batch.sanitized_transactions())
+        .filter_map(|(commit_result, tx)| commit_result.was_committed().then_some(tx))
+    {
+        unsafe extern "C" {
+            fn fd_ext_poh_publish_executed_txn(data: *const u8);
+        }
+
+        let mut memory: [u8; 64] = [0u8; 64];
+        memory.copy_from_slice(txn.signature().as_ref());
+        unsafe { fd_ext_poh_publish_executed_txn(memory.as_ptr()) };
+    }
+
     if let Some(prioritization_fee_cache) = prioritization_fee_cache {
         let fee_paying_transactions = commit_results
             .iter()
