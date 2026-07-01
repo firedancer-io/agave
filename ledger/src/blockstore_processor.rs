99344b9a58b3e9d560c59873f59f746574f75504
Manik Jain <mjain@jumptrading.com>
2026-07-01 22:36:47 +0000

    blockstore: add pub qualifier to verify_ticks function
    (cherry picked from commit de326f796904afad8f43e37c46a8bd7fe7dde46a)

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -1064,6 +1064,7 @@ pub fn process_blockstore_from_root(
 }
 
 /// Verify that a segment of entries has the correct number of ticks and hashes
+#[cfg_attr(feature = "dev-context-only-utils", qualifiers(pub))]
 fn verify_ticks(
     bank: &Bank,
     entries: &[Entry],
